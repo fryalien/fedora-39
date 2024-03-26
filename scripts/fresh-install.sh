ec2f87f82c64ab0a8619e43b89791871b562a5c4
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2024-03-26 12:02:43 +0000

    Update fresh-install.sh

--- scripts/fresh-install.sh
+++ scripts/fresh-install.sh
@@ -1,29 +1,27 @@
 #!/bin/sh
 
-sudo cat ./scripts/dnf.add >> /etc/dnf/dnf.conf
+cat ./scripts/dnf.add >> /etc/dnf/dnf.conf
 
 # UPDATE REPOSITORIES AND INSTALL UPDATES
-sudo dnf update --refresh
+yes | dnf update --refresh
 
 # INSTALL RPMFUSION FREE
-sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+yes | dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
 # INSTALL RPM-FUSION NON-FREE
-sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+yes | dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # INSTALL OVER GUI
-sudo dnf group update core
+dnf group update core -y
 
 # INSTALL FLATPAK
-sudo dnf install -y flatpak
+dnf install -y flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # INSTALL CODECS ON FEDORA
-sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
-sudo dnf install -y lame\* --exclude=lame-devel
-sudo dnf group upgrade --allowerasing --with-optional Multimedia
+dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+dnf install -y lame\* --exclude=lame-devel
+dnf group upgrade --allowerasing --with-optional Multimedia -y
 
 # INSTALL FISH SHELL
-sudo dnf install -y fish
-chsh -s /usr/bin/fish
-
+dnf install -y fish
