505aef72a7f087e6eb1bc9462641a386c2965e75
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2024-03-26 12:13:06 +0000

    Update applauncher.sh

--- scripts/applauncher.sh
+++ scripts/applauncher.sh
@@ -2,8 +2,6 @@
 
 # DOWNLOAD APPIMAGELAUNCHER
 
-cd ~/Downloads
-
 wget https://github.com/TheAssassin/AppImageLauncher/releases/download/continuous/appimagelauncher-2.2.0-gha111.d9d4c73.x86_64.rpm
 
 yes | rpm -i appimagelauncher-2.2.0-gha111.d9d4c73.x86_64.rpm
